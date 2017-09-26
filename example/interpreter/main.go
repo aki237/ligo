30e5bb7c7908859db3ba5b48a3a5275c8c48ea99
Akilan Elango <akilan1997@gmail.com>
2017-09-26 22:37:10 +0530

    example/interpreter/main.go : removed some code related to auxillary functions referenced in commit f85447738659744e11ba2bf9b13baba51b471b63
    (Note : this file is going to be a command for the interpreter)

--- example/interpreter/main.go
+++ example/interpreter/main.go
@@ -4,73 +4,122 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"plugin"
 
 	"github.com/aki237/ligo"
 )
 
+var packages = make([]string, 0)
+
 func run() {
 	vm := ligo.NewVM()
-	vm.Funcs["println"] = vmPrintln
-	vm.Funcs["print"] = vmPrint
-	vm.Funcs["=="] = vmEquality
-	vm.Funcs["car"] = vmCar
-	vm.Funcs["cdr"] = vmCdr
-	vm.Funcs["len"] = vmLen
-	vm.Funcs[">="] = vmInEqualityGTEQ
-	vm.Funcs["<="] = vmInEqualityLTEQ
-	vm.Funcs[">"] = vmInEqualityGT
-	vm.Funcs["+"] = vmAdd
-	vm.Funcs["*"] = vmProd
-	vm.Funcs["%"] = vmModulus
-	vm.Funcs["type"] = vmType
-	vm.Funcs["panic"] = vmPanic
-	vm.Funcs["sleep"] = vmSleep
-	vm.Funcs["open"] = vmOpen
-	vm.Funcs["read"] = vmRead
-	vm.Funcs["write"] = vmWrite
-	vm.Funcs["split"] = vmSplit
-	vm.Funcs["net.connect"] = vmConnect
-
+	vm.Funcs["require"] = VMRequire
+	vm.Funcs["load-plugin"] = VMDlLoad
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage %s [filename.lf]\n", os.Args[0])
 		return
 	}
-	ltxtb, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error : %s\n", err)
-		return
+	fmt.Println(vm.LoadFile(os.Args[1]))
+}
+
+func VMRequire(vm *ligo.VM, a ...ligo.Variable) ligo.Variable {
+	if len(a) != 1 {
+		panic("require : wrong number of arguments")
 	}
-	ltxt := string(ltxtb)
-	exps := make([]string, 0)
-	line := 0
-	for i := 0; i < len(ltxt); i++ {
-		ch := string(ltxt[i])
-		switch ch {
-		case "(":
-			off := ligo.MatchChars(ltxt, int64(i), '(', ')') + 1
-			exps = append(exps, ltxt[i:off])
-			i = int(off)
-		case " ", "\n", "\r", "\t":
-			if ch == "\n" || ch == "\r" {
-				line += 1
-			}
-			continue
-		default:
-			fmt.Fprintf(os.Stderr, "Unexpected Character at line %d : %s\n", line, ch)
-			return
-		}
+	lib := a[0]
+	if lib.Type != ligo.TYPE_String {
+		panic("require : expected a string, got " + lib.GetTypeString())
 	}
 
-	for _, val := range exps {
-		vl, err := vm.Eval(val)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error : %s\n", err)
-			break
+	packageName := lib.Value.(string)
+	err := LoadPackage(vm, packageName)
+	if err != nil {
+		panic(err)
+	}
+	return ligo.Variable{Type: ligo.TYPE_Nil, Value: nil}
+}
+
+func VMDlLoad(vm *ligo.VM, a ...ligo.Variable) ligo.Variable {
+	if len(a) != 1 {
+		panic("load-plugin can only take one argument")
+	}
+
+	libpath := a[0].Value.(string)
+
+	p, err := plugin.Open(libpath)
+	if err != nil {
+		panic(err)
+	}
+	init, err := p.Lookup("PluginInit")
+	if err != nil {
+		panic(err)
+	}
+
+	init.(func(*ligo.VM))(vm)
+
+	return ligo.Variable{Type: ligo.TYPE_Nil, Value: nil}
+}
+
+func exists(dir string) bool {
+	if _, err := os.Stat(dir); !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+func LoadPackage(vm *ligo.VM, packageName string) error {
+	if slistContains(packages, packageName) {
+		return nil
+	}
+	home := os.Getenv("HOME")
+	dir := filepath.Join(home, "lispace", "lib", packageName)
+	if !exists(dir) {
+		return ligo.LigoError("Package \"" + packageName + "\" not found in the system")
+	}
+	if info, _ := os.Stat(dir); !info.IsDir() {
+		return ligo.LigoError("Package \"" + packageName + "\" is not a valid directory")
+	}
+
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic("require : " + fmt.Sprint(err))
+	}
+	for _, val := range fileInfos {
+		if val.IsDir() {
+			continue
 		}
-		if vl.Type != ligo.TYPE_Nil {
-			fmt.Println(vl.Value)
+		if filepath.Ext(val.Name()) != ".lg" {
+			if filepath.Ext(val.Name()) == ".plg" {
+				p, err := plugin.Open(filepath.Join(dir, val.Name()))
+				if err != nil {
+					panic(err)
+				}
+				init, err := p.Lookup("PluginInit")
+				if err != nil {
+					panic(err)
+				}
+
+				init.(func(*ligo.VM))(vm)
+			}
+			continue
+		}
+		err := vm.LoadFile(filepath.Join(dir, val.Name()))
+		if err != nil {
+			panic(err)
 		}
 	}
+	packages = append(packages, packageName)
+	return nil
+}
+
+func slistContains(sl []string, s string) bool {
+	for _, val := range sl {
+		if val == s {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {

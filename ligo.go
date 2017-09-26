b5b3ad09e052081315f35ec0be43c3b734612706
Akilan Elango <akilan1997@gmail.com>
2017-09-26 22:16:10 +0530

    ligo.go : this commit is a huge commit. Sorry for load of changes  + renamed regexp pattern variables from snake case to camel case  + start to reform escape sequences with a function and a string list  + GetTypeString is used to get the type of a variable as a string  + GetVariable : using match chars as a validator along with regexp patterns  + Added support for variate parameters to a function and decoupling arrays to variate parameters  + introduced a new type of loop in loop  + function for deleting variables  + LoadFile function to load file is merged in the package

--- ligo.go
+++ ligo.go
@@ -2,22 +2,91 @@ package ligo
 
 import (
 	"fmt"
+	"io/ioutil"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-var r_integer *regexp.Regexp = regexp.MustCompile("^[+-]?[0-9]+$")
-var r_float *regexp.Regexp = regexp.MustCompile("^[+-]?[0-9]*\\.[0-9]+$")
-var r_string *regexp.Regexp = regexp.MustCompile("^\".*\"$")
-var r_variable *regexp.Regexp = regexp.MustCompile("^[[:alpha:]]+[[:alnum:]]*$")
-var r_expression *regexp.Regexp = regexp.MustCompile("^\\(.*\\)$")
-var r_closure *regexp.Regexp = regexp.MustCompile("^\\|([[:alpha:]]+[[:alnum:]]*\\s*)*\\|$")
-var r_array *regexp.Regexp = regexp.MustCompile("^\\[(.*\\s*)*\\]$")
+var escapeSequences = map[string]int{
+	"\\\\": 0x5C,
+	"\\a":  0x07,
+	"\\b":  0x08,
+	"\\f":  0x0C,
+	"\\n":  0x0A,
+	"\\r":  0x0D,
+	"\\t":  0x09,
+	"\\v":  0x0B,
+	"\\'":  0x27,
+	"\"":   0x22,
+}
+
+// TODO : add escape sequence handling for hex and octal sequences...
+func reformEscapes(str string) string {
+	ret := ""
+	isEscape := false
+	for _, val := range str {
+		switch val {
+		case '\\':
+			if !isEscape {
+				isEscape = true
+			} else {
+				ret += string(0x5C)
+				isEscape = false
+			}
+		default:
+			if !isEscape {
+				ret += string(val)
+			} else {
+				es := "\\" + string(val)
+				num, ok := escapeSequences[es]
+				if !ok {
+					panic("in :\n\t" + str + "\nUnknown Escape sequence. : '\\" + string(val) + "'")
+				}
+				ret += string(num)
+				isEscape = false
+			}
+		}
+	}
+	return ret
+}
+
+var rInteger = regexp.MustCompile("^[+-]?[0-9]+$")
+var rFloat = regexp.MustCompile("^[+-]?[0-9]*\\.[0-9]+$")
+var rString = regexp.MustCompile("^\".*\"$")
+var rVariable = regexp.MustCompile("^[[:alpha:]]+[[:alnum:]]*$")
+var rExpression = regexp.MustCompile("^\\(.*\\)$")
+var rClosure = regexp.MustCompile("^\\|([[:alpha:]]+[[:alnum:]]*\\s*)*\\.\\.\\.([[:alpha:]]+[[:alnum:]]*\\s*){0,1}|$")
+var rArray = regexp.MustCompile("^\\[(.*\\s*)*\\]$")
 
 type Variable struct {
 	Type  Type
 	Value interface{}
+}
+
+func (v Variable) GetTypeString() (tp string) {
+	tp = ""
+	switch v.Type {
+	case TYPE_Int:
+		tp = "int"
+	case TYPE_Float:
+		tp = "float"
+	case TYPE_Bool:
+		tp = "bool"
+	case TYPE_String:
+		tp = "string"
+	case TYPE_Nil:
+		tp = "nil"
+	case TYPE_DFunc, TYPE_IFunc:
+		tp = "func"
+	case TYPE_MonoTypeArray:
+		tp = "array<mono type>"
+	case TYPE_PolyTypeArray:
+		tp = "array<poly type>"
+	case TYPE_Map:
+		tp = "map"
+	}
+	return
 }
 
 func (v Variable) String() string {
@@ -70,7 +139,7 @@ func (vm *VM) GetVariable(token string) (Variable, error) {
 		return ligoNil, LigoError("Invalid Token passed")
 	}
 	switch true {
-	case r_array.MatchString(token):
+	case rArray.MatchString(token):
 		ar := token[1:MatchChars(token, 0, '[', ']')]
 		tkns, err := ScanTokens("(" + ar + ")")
 		if err != nil {
@@ -97,28 +166,29 @@ func (vm *VM) GetVariable(token string) (Variable, error) {
 		}
 		retVars := Variable{Type: tp, Value: vars}
 		return retVars, nil
-	case r_expression.MatchString(token) || token[0] == '(':
+	case rExpression.MatchString(token) || MatchChars(token, 0, '(', ')') > 0:
 		var err error
 		v, err = vm.Eval(token)
 		if err != nil {
 			return ligoNil, err
 		}
-	case r_integer.MatchString(token):
+	case rInteger.MatchString(token):
 		num, err := strconv.ParseInt(token, 10, 64)
 		if err != nil {
 			fmt.Println(err)
 		}
 		v = Variable{Type: TYPE_Int, Value: num}
-	case r_float.MatchString(token):
+	case rFloat.MatchString(token):
 		num, _ := strconv.ParseFloat(token, 64)
 		v = Variable{Type: TYPE_Float, Value: num}
-	case r_string.MatchString(token) || token[0] == '"':
+	case rString.MatchString(token) || token[0] == '"':
+		token = reformEscapes(token)
 		v = Variable{Type: TYPE_String, Value: token[1 : len(token)-1]}
 	case token == "true":
 		v = Variable{Type: TYPE_Bool, Value: true}
 	case token == "false":
 		v = Variable{Type: TYPE_Bool, Value: false}
-	case r_variable.MatchString(token):
+	case rVariable.MatchString(token):
 		varFromVM, ok := vm.Vars[token]
 		if ok {
 			v = Variable{Type: varFromVM.Type, Value: varFromVM.Value}
@@ -160,7 +230,7 @@ func (vm *VM) setFn(tokens []string) (Variable, error) {
 	if _, ok := vm.LFuncs[fnName]; ok {
 		fmt.Printf("Warning : function \"%s\" has already been declared as an Ligo function.\n", fnName)
 	}
-	if !r_closure.MatchString(tokens[2]) {
+	if !rClosure.MatchString(tokens[2]) {
 		return ligoNil,
 			LigoError("Expected parameter name in the function definition " + fnName + " closure : " + tokens[2])
 	}
@@ -174,7 +244,7 @@ func (vm *VM) setVar(tokens []string) (Variable, error) {
 	if len(tokens) != 3 {
 		return ligoNil, LigoError("Wrong number of arguments to the keyword.")
 	}
-	if !r_variable.MatchString(tokens[1]) {
+	if !rVariable.MatchString(tokens[1]) {
 		return ligoNil, LigoError("Wrong token found in the variable name")
 	}
 	v, err := vm.GetVariable(tokens[2])
@@ -214,7 +284,7 @@ func (vm *VM) newVar(tokens []string) (Variable, error) {
 	if len(tokens) != 3 {
 		return ligoNil, LigoError("Wrong number of arguments to the keyword.")
 	}
-	if !r_variable.MatchString(tokens[1]) {
+	if !rVariable.MatchString(tokens[1]) {
 		return ligoNil, LigoError("Wrong token found in the variable name")
 	}
 	v, err := vm.GetVariable(tokens[2])
@@ -251,14 +321,32 @@ func (vm *VM) getDefinedFunction(fnName string) (Defined, bool) {
 	return fn, found
 }
 
+func isVariate(str string) bool {
+	if len(str) > 4 && str[:3] == "..." && str[3] != '.' {
+		return true
+	}
+	return false
+}
+
 func (vm *VM) runDefinedFunction(function Defined, fnName string, vars []Variable) (Variable, error) {
-	if len(vars) != len(function.scopevars) {
+	if len(vars) < len(function.scopevars)-1 {
 		return ligoNil, LigoError(fmt.Sprintf("Expected %d arguments, got %d for the %s function",
 			len(function.scopevars),
 			len(vars),
 			fnName,
 		))
 	}
+
+	if len(function.scopevars) > 0 && !isVariate(function.scopevars[len(function.scopevars)-1]) {
+		if len(vars) != len(function.scopevars) {
+			return ligoNil, LigoError(fmt.Sprintf("Expected %d arguments, got %d for the %s function",
+				len(function.scopevars),
+				len(vars),
+				fnName,
+			))
+		}
+	}
+
 	nvm := vm.NewScope()
 	for i, val := range function.scopevars {
 		switch vars[i].Type {
@@ -267,6 +355,16 @@ func (vm *VM) runDefinedFunction(function Defined, fnName string, vars []Variabl
 		case TYPE_DFunc:
 			nvm.LFuncs[val] = vars[i].Value.(Defined)
 		default:
+			if isVariate(val) {
+				if len(function.scopevars) != i+1 {
+					panic(fmt.Sprintf("In function %s, the variate parameter should be at the end", fnName))
+				}
+				val = val[3:]
+				if len(vars) >= i+1 {
+					nvm.Vars[val] = Variable{TYPE_MonoTypeArray, vars[i:]}
+				}
+				break
+			}
 			nvm.Vars[val] = vars[i]
 		}
 	}
@@ -277,11 +375,24 @@ func (vm *VM) run(tkns []string) (Variable, error) {
 	vars := make([]Variable, 0)
 	fnName := tkns[0]
 	for i := 1; i < len(tkns); i++ {
+
+		if len(tkns[i]) > 3 && tkns[i][:3] == "..." && tkns[i][3] != '.' {
+			v, err := vm.GetVariable(tkns[i][3:])
+			if err != nil {
+				return ligoNil, err
+			}
+			if v.Type == TYPE_MonoTypeArray || v.Type == TYPE_PolyTypeArray {
+				vars = append(vars, v.Value.([]Variable)...)
+				continue
+			}
+			vars = append(vars, v)
+			continue
+		}
+
 		v, err := vm.GetVariable(tkns[i])
 		if err != nil {
 			return ligoNil, err
 		}
-
 		vars = append(vars, v)
 	}
 	if function, ok := vm.getInBuiltFunction(fnName); ok {
@@ -332,13 +443,55 @@ func (vm *VM) runLoop(tkns []string) (Variable, error) {
 	return ligoNil, err
 }
 
+func (vm *VM) runIn(tkns []string) (Variable, error) {
+	if len(tkns) != 4 {
+		return ligoNil, LigoError("Illegal in loop construct. Can take 4 arguments only.")
+	}
+	iterVar := tkns[2]
+	runExp := tkns[3]
+
+	array, err := vm.GetVariable(tkns[1])
+	if err != nil {
+		return ligoNil, err
+	}
+
+	if array.Type != TYPE_String && array.Type != TYPE_PolyTypeArray && array.Type != TYPE_MonoTypeArray {
+		return ligoNil, LigoError("in : can only iterate thorugh arrays or strings")
+	}
+
+	v, ok := vm.Vars[iterVar]
+	if array.Type == TYPE_String {
+		for _, val := range array.Value.(string) {
+			vm.Vars[iterVar] = Variable{Type: TYPE_String, Value: string(val)}
+			_, err = vm.Eval(runExp)
+			if err != nil {
+				return ligoNil, err
+			}
+		}
+	} else {
+		for _, val := range array.Value.([]Variable) {
+			vm.Vars[iterVar] = val
+			_, err = vm.Eval(runExp)
+			if err != nil {
+				return ligoNil, err
+			}
+		}
+	}
+	if ok {
+		vm.Vars[iterVar] = v
+	} else {
+		delete(vm.Vars, iterVar)
+	}
+	return ligoNil, nil
+}
+
 func (vm *VM) ifClause(tkns []string) (Variable, error) {
 	if len(tkns) > 4 || len(tkns) < 3 {
 		return ligoNil, LigoError("Illegal if construct. Can take 3 or 4 arguments.")
 	}
 	condition := tkns[1]
 	boolVar, ok := vm.Vars[condition]
-	if condition != "true" && condition != "false" && !r_expression.MatchString(condition) && !ok {
+	if condition != "true" && condition != "false" && MatchChars(condition, 0, '(', ')') < 0 && !ok {
 		return ligoNil,
 			LigoError("Expected a boolean value or expression for the if clause condition, got : " + condition)
 	}
@@ -375,6 +528,20 @@ func (vm *VM) returnArg(tkns []string) (Variable, error) {
 		panic("Cannot return more than 2 values. (Atleast for now.)")
 	}
 	return vm.GetVariable(tkns[1])
+}
+
+func (vm *VM) deleteVar(tkns []string) (Variable, error) {
+	if len(tkns) < 2 {
+		return Variable{Type: TYPE_Bool, Value: false}, LigoError("nothing passed to delete")
+	}
+	for _, variable := range tkns[1:] {
+		_, ok := vm.Vars[variable]
+		if !ok {
+			return Variable{Type: TYPE_Bool, Value: false}, LigoError("variable not found")
+		}
+		delete(vm.Vars, variable)
+	}
+	return Variable{Type: TYPE_Bool, Value: true}, nil
 }
 
 func (vm *VM) fork(tkns []string) (Variable, error) {
@@ -448,7 +615,7 @@ func (vm *VM) Eval(stmt string) (Variable, error) {
 	if len(stmt) < 2 {
 		return ligoNil, LigoError("Expected atleast (), got : " + stmt)
 	}
-	if !r_expression.MatchString(stmt) && stmt[0] != '(' {
+	if !rExpression.MatchString(stmt) && MatchChars(stmt, 0, '(', ')') < 0 {
 		return vm.GetVariable(stmt)
 	}
 	tkns, err := ScanTokens(stmt)
@@ -459,6 +626,7 @@ func (vm *VM) Eval(stmt string) (Variable, error) {
 		return ligoNil, nil
 	}
 	fnName := tkns[0]
+
 	switch fnName {
 	case "var":
 		return vm.newVar(tkns)
@@ -472,12 +640,16 @@ func (vm *VM) Eval(stmt string) (Variable, error) {
 		return vm.runExpressions(tkns)
 	case "loop":
 		return vm.runLoop(tkns)
+	case "in":
+		return vm.runIn(tkns)
 	case "if":
 		return vm.ifClause(tkns)
 	case "eval":
 		return vm.evalString(tkns)
 	case "fork":
 		return vm.fork(tkns)
+	case "delete":
+		return vm.deleteVar(tkns)
 	default:
 		return vm.run(tkns)
 	}
@@ -506,4 +678,52 @@ func (vm *VM) NewScope() *VM {
 		nvm.global = vm.global
 	}
 	return nvm
+}
+
+func (vm *VM) LoadFile(fileName string) error {
+	ltxtb, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+	ltxt := StripComments(string(ltxtb))
+	exps := make([]string, 0)
+	line := 0
+	inComment := false
+	for i := 0; i < len(ltxt); i++ {
+		ch := string(ltxt[i])
+		switch ch {
+		case "(":
+			if inComment {
+				continue
+			}
+			off := MatchChars(ltxt, int64(i), '(', ')') + 1
+			if off < int64(i) {
+				return fmt.Errorf("Syntax error near %d:%d : %s", i, line, ltxt[i:])
+			}
+			exps = append(exps, ltxt[i:off])
+			i = int(off) - 1
+		case " ", "\n", "\r", "\t":
+			if ch == "\n" || ch == "\r" {
+				line++
+				inComment = false
+			}
+			continue
+		case ";":
+			inComment = true
+		default:
+			if inComment {
+				continue
+			}
+			return fmt.Errorf("unexpected Character at line %d : %s", line, ch)
+		}
+	}
+
+	for _, val := range exps {
+		_, err := vm.Eval(val)
+		if err != nil {
+			return fmt.Errorf("error : %s", err)
+		}
+	}
+
+	return nil
 }

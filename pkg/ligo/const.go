bf9ce29553492b12f6a03e378597ece58b1cd9f1
Akilan Elango <akilan1997@gmail.com>
2017-11-12 09:10:19 +0530

    pkg/ligo/const.go : removed some unused types and added a new type for representing structs

--- pkg/ligo/const.go
+++ pkg/ligo/const.go
@@ -14,26 +14,18 @@ type Type int
 
 // Required constants for the variable type
 const (
-	TypeErr             Type = -0x00
-	TypeInt             Type = 0x000
-	TypeFloat           Type = 0x001
-	TypeBool            Type = 0x002
-	TypeString          Type = 0x003
-	TypeNil             Type = 0x004
-	TypeIFunc           Type = 0x005
-	TypeDFunc           Type = 0x006
-	TypeExp             Type = 0x007
-	TypeArray           Type = 0x100
-	TypeMap             Type = 0x300
-	TypeReader          Type = 0x400
-	TypeWriter          Type = 0x500
-	TypeSeeker          Type = 0x600
-	TypeCloser          Type = 0x700
-	TypeReadCloser      Type = 0x701
-	TypeReadSeeker      Type = 0x702
-	TypeReadWriter      Type = 0x703
-	TypeReadWriteSeeker Type = 0x704
-	TypeReadWriteCloser Type = 0x705
+	TypeErr    Type = -0x00
+	TypeInt    Type = 0x000
+	TypeFloat  Type = 0x001
+	TypeBool   Type = 0x002
+	TypeString Type = 0x003
+	TypeNil    Type = 0x004
+	TypeIFunc  Type = 0x005
+	TypeDFunc  Type = 0x006
+	TypeExp    Type = 0x007
+	TypeArray  Type = 0x100
+	TypeMap    Type = 0x300
+	TypeStruct Type = 0x400
 )
 
 var ligoNil = Variable{TypeNil, nil}
